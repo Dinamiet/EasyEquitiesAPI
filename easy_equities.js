92c17039fcc1620b041ca7811488e03bd63353a1
Dinamiet <iceman.sm39@gmail.com>
2025-04-30 22:07:18 +0200

    Add getHoldings function

--- easy_equities.js
+++ easy_equities.js
@@ -1,16 +1,35 @@
 const puppeteer = require('puppeteer');
+const path = require('path');
+
+const extractNumber = (currencyString) => {
+	// Regular expression to find a floating-point number
+	// It looks for:
+	// - An optional minus sign at the beginning: -?
+	// - One or more digits: \d+
+	// - An optional decimal part: (\.\d+)?
+	const floatRegex = /-?\d+(\s+\d+)?(\.\d+)?/;
+
+	const match = currencyString.match(floatRegex);
+
+	if (match) {
+		return parseFloat(match[0].replace(/\s/, ''));
+	}
+
+	return 0;
+};
 
 class EasyEquities {
 	constructor(username, password) {
 		this.authentication = { username: username, password: password };
 		this.options = {
 			headless: false,
+			devtools: true,
 		};
 		this.browser = null;
 	}
 
 	async Initialize() {
-		if (this.browser == null) {
+		if (this.browser === null) {
 			try {
 				this.browser = await puppeteer.launch(this.options);
 			} catch (error) {
@@ -50,11 +69,46 @@ class EasyEquities {
 
 		const newUrl = page.url();
 
-		if (newUrl.startsWith(loginUrl)) {
-			return false;
-		}
+		page.close();
+		return !newUrl.startsWith(loginUrl);
+	}
 
-		return true;
+	async GetHoldings(accountID) {
+		await this.Initialize();
+		const page = await this.browser.newPage();
+
+		await page.goto("https://platform.easyequities.io/AccountOverview");
+
+		const accountTab = `div[data-id='${accountID}']`;
+		await page.waitForSelector(accountTab);
+		await page.click(accountTab);
+		await page.waitForNavigation({ waitUntil: 'networkidle0' });
+
+		await page.waitForSelector("button#loadHoldings");
+		await page.click("button#loadHoldings");
+
+		const holdingCellSelector = "div.img-stocks-container > img";
+		const purchaseCellSelector = "div.purchase-value-cell > span";
+		const currentCellSelector = "div.current-value-cell > span";
+
+		await page.waitForSelector(holdingCellSelector);
+		await page.waitForSelector(purchaseCellSelector);
+		await page.waitForSelector(currentCellSelector);
+
+		const [holdings, purchases, currents] = await Promise.all([
+			page.$$eval(holdingCellSelector, elements => elements.map(element => element.getAttribute('src'))),
+			page.$$eval(purchaseCellSelector, elements => elements.map(element => element.textContent)),
+			page.$$eval(currentCellSelector, elements => elements.map(element => element.textContent)),
+		]);
+
+		const info = holdings.map((symbol, index) => ({
+			symbol: path.basename(symbol).split('.')[2],
+			purchase: extractNumber(purchases[index]),
+			current: extractNumber(currents[index]),
+		}));
+
+		page.close();
+		return info;
 	}
 };
 
